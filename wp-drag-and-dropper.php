6fc85f2571383b241a3c140c1229061accbba56f
Gilad Ehven <gilad@ehven.com>
2018-05-22 17:08:08 -0400

    First localhost commit: Base file system (including untested [composer.json]); parallels 'wordpress-plugin-starter-type-four'.

---
+++ wp-drag-and-dropper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * WordPress plugin that adds drag-and-drop surfaces to various screens (for key default and custom types)
+ *
+ * @author                  Gilad Ehven
+ * @copyright               2018 Gilad Ehven (Ehventerprise LLC)
+ * @license                 GNU General Public License 2.0+
+ * @link                    https://github.com/GiladEhven/wp-drag-and-dropper
+ * @package                 WpDragAndDropper
+ * @since                   0.1.0
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name:             WP Drag and Dropper
+ *
+ * Author:                  Gilad Ehven
+ * Author URI:              http://gilad-ehven.com
+ * Contributors:            Mihal Ehven (Ehventerprise LLC)
+ * Description:             This plugin adds drag-and-drop surfaces to various screens (for key default and custom types).
+ * Domain Path:             /languages
+ * License:                 GNU General Public License 2.0+
+ * License URI:             http://www.gnu.org/licenses/gpl-2.0.txt
+ * Network:                 False
+ * Plugin URI:              https://github.com/GiladEhven/wp-drag-and-dropper
+ * Privacy Policy:          https://gilad-ehven.com/privacy/
+ * Terms of Use:            https://gilad-ehven.com/terms/
+ * Text Domain:             wp-drag-and-dropper
+ * Version:                 0.1.0
+ *
+ */
+
+namespace WpDragAndDropper;
+
+if ( ! defined( 'ABSPATH' ) ) exit( "Nothing to see here. Sorry. Try the home page." );
